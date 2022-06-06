85f233819992e748ca6fd59723ed477da9527a54
Steve Ngo <stevehngo@hotmail.com>
2022-06-06 01:18:30 -0400

    tests

--- cattle/tests.py
+++ cattle/tests.py
@@ -3,10 +3,10 @@ from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
 
-from .models import Thing
+from .models import Cattle
 
 
-class ThingTests(APITestCase):
+class CattleTests(APITestCase):
     @classmethod
     def setUpTestData(cls):
         testuser1 = get_user_model().objects.create_user(
@@ -14,50 +14,50 @@ class ThingTests(APITestCase):
         )
         testuser1.save()
 
-        test_thing = Thing.objects.create(
+        test_cattle = Cattle.objects.create(
             name="rake",
             owner=testuser1,
             description="Better for collecting leaves than a shovel.",
         )
-        test_thing.save()
+        test_cattle.save()
 
-    def test_things_model(self):
-        thing = Thing.objects.get(id=1)
-        actual_owner = str(thing.owner)
-        actual_name = str(thing.name)
-        actual_description = str(thing.description)
+    def test_cattles_model(self):
+        cattle = Cattle.objects.get(id=1)
+        actual_owner = str(cattle.owner)
+        actual_name = str(cattle.name)
+        actual_description = str(cattle.description)
         self.assertEqual(actual_owner, "testuser1")
         self.assertEqual(actual_name, "rake")
         self.assertEqual(
             actual_description, "Better for collecting leaves than a shovel."
         )
 
-    def test_get_thing_list(self):
-        url = reverse("thing_list")
+    def test_get_cattle_list(self):
+        url = reverse("cattle_list")
         response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        things = response.data
-        self.assertEqual(len(things), 1)
-        self.assertEqual(things[0]["name"], "rake")
+        cattles = response.data
+        self.assertEqual(len(cattles), 1)
+        self.assertEqual(cattles[0]["name"], "rake")
 
-    def test_get_thing_by_id(self):
-        url = reverse("thing_detail", args=(1,))
+    def test_get_cattle_by_id(self):
+        url = reverse("cattle_detail", args=(1,))
         response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        thing = response.data
-        self.assertEqual(thing["name"], "rake")
+        cattle = response.data
+        self.assertEqual(cattle["name"], "rake")
 
-    def test_create_thing(self):
-        url = reverse("thing_list")
+    def test_create_cattle(self):
+        url = reverse("cattle_list")
         data = {"owner": 1, "name": "spoon", "description": "good for cereal and soup"}
         response = self.client.post(url, data)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        things = Thing.objects.all()
-        self.assertEqual(len(things), 2)
-        self.assertEqual(Thing.objects.get(id=2).name, "spoon")
+        cattles = Cattle.objects.all()
+        self.assertEqual(len(cattles), 2)
+        self.assertEqual(Cattle.objects.get(id=2).name, "spoon")
 
-    def test_update_thing(self):
-        url = reverse("thing_detail", args=(1,))
+    def test_update_cattle(self):
+        url = reverse("cattle_detail", args=(1,))
         data = {
             "owner": 1,
             "name": "rake",
@@ -65,14 +65,14 @@ class ThingTests(APITestCase):
         }
         response = self.client.put(url, data)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        thing = Thing.objects.get(id=1)
-        self.assertEqual(thing.name, data["name"])
-        self.assertEqual(thing.owner.id, data["owner"])
-        self.assertEqual(thing.description, data["description"])
+        cattle = Cattle.objects.get(id=1)
+        self.assertEqual(cattle.name, data["name"])
+        self.assertEqual(cattle.owner.id, data["owner"])
+        self.assertEqual(cattle.description, data["description"])
 
-    def test_delete_thing(self):
-        url = reverse("thing_detail", args=(1,))
+    def test_delete_cattle(self):
+        url = reverse("cattle_detail", args=(1,))
         response = self.client.delete(url)
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
-        things = Thing.objects.all()
-        self.assertEqual(len(things), 0)
+        cattles = Cattle.objects.all()
+        self.assertEqual(len(cattles), 0)
